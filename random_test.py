9ef829b367cca6a72957ee8d68eef6e4d8ccb074
Anand <anand04karthick@gmail.com>
2018-06-10 13:49:12 -0400

    addition, random forest..

---
+++ random_test.py
@@ -0,0 +1,3 @@
+import random_train as rt
+
+result = rt.random_train('train_data')
---
+++ random_test_predict.py
@@ -0,0 +1,6 @@
+import random_predict as rp
+
+phrase = "What's your name"
+
+result = rp.random_predict(phrase)
+
